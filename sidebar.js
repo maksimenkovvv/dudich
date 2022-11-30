497c0a8c87f31ea00eb4b1755d1e909993fbb1df
Anton Maksimenkov <urbangorod888@gmail.com>
2022-11-30 09:42:45 +0300

    remove scroll from popup menu

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,9 @@
 let popupBtn = document.querySelector('.popup__button');
 let menu = document.querySelector('.left__sidebar');
+let noScroll = document.querySelector('html');
 
 popupBtn.addEventListener('click', function(){
     popupBtn.classList.toggle('active');
     menu.classList.toggle('active');
-    $('html').addClass('.no-scroll');
+    noScroll.classList.toggle('no-scroll');
 })
