490d6440e361d366dc9b688646680294da161cab
Anton Maksimenkov <urbangorod888@gmail.com>
2022-12-01 13:27:33 +0300

    99% done

---
+++ services.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Спа-салон "Aqua" | Услуги</title>
+  <link rel="stylesheet" href="style.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="container">
+    <div class="left__sidebar">
+      <div class="popup__menu__container">
+        <div class="popup__menu">
+          <a href="index.html" class="popup__menu__pages">Главная</a>
+          <a href="about.html" class="popup__menu__pages">О нас</a>
+          <a href="services.php" class="popup__menu__pages">Услуги</a>
+        </div>
+      </div>
+    </div>
+    <div class="right__sidebar">
+      <div class="menu">
+        <div class="popup__button__container">
+          <div class="popup__button">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="logo__container">
+          <img src="./img/aqua_logo.png" alt="Лого" class="logo__image" />
+          <h2 class="logo__text">| Услуги</h2>
+        </div>
+        <div class="menu__nav">
+          <a href="index.html" class="menu__nav__pages">Главная</a>
+          <a href="about.html" class="menu__nav__pages">О нас</a>
+          <a href="services.php" class="menu__nav__pages">Услуги</a>
+        </div>
+      </div>
+      <div class="card__container">
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/банярус.jpg" alt="Русская баня" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-1">
+            <p class="card__text card__text-1">
+              Русская баня | 1 час | 1000₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/банятур.jpeg" alt="Турецкая баня" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-2">
+            <p class="card__text card__text-2">
+              Турецкая баня | 1 час | 1000₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/баняфин.jpg" alt="Финская баня" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-3">
+            <p class="card__text card__text-3">
+              Финская баня | 1 час | 1200₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/массаж.jpg" alt="Массаж" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-4">
+            <p class="card__text card__text-4">Массаж | 30 минут | 600₽</p>
+          </div>
+        </div>
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/массажкам.jpg" alt="Массаж камнями" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-5">
+            <p class="card__text card__text-5">
+              Массаж камнями | 40 минут | 800₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/вакууммассаж.jpg" alt="Вакуумный массаж" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-6">
+            <p class="card__text card__text-6">
+              Вакуумный массаж | 20 минут | 1000₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/гидромассаж.jpg" alt="Гидромассаж" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-7">
+            <p class="card__text card__text-7">
+              Гидромассаж | 45 минут | 1500₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/грванны.jpg" alt="Грязевые ванны" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-8">
+            <p class="card__text card__text-8">
+              Грязевые ванны | 1 час | 3500₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/душмарко.jpg" alt="Душ марко" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-9">
+            <p class="card__text card__text-9">Душ Марко | 10 минут | 300₽</p>
+          </div>
+        </div>
+        <div class="card card__leftside popup-open">
+          <div class="card__image-container">
+            <img src="./img/инфрсауна.jpeg" alt="Инфракрасная сауна" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-10">
+            <p class="card__text card__text-10">
+              Инфракрасная сауна | 30 минут | 2000₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/спакапсула.jpg" alt="Спа-капсула" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-11">
+            <p class="card__text card__text-11">
+              Спа-капсула | 30 минут | 1500₽
+            </p>
+          </div>
+        </div>
+        <div class="card card__rightside popup-open">
+          <div class="card__image-container">
+            <img src="./img/солярий.jpg" alt="Солярий" class="card__image image" />
+          </div>
+          <div class="card__text-container card__text-container-12">
+            <p class="card__text card__text-12">Солярий | 20 минут | 1700₽</p>
+          </div>
+        </div>
+      </div>
+      <div class="popup-bg">
+        <div class="popup">
+          <img src="./img/close.png" alt="Закрыть" class="popup-close" />
+          <div class="popup__header">
+            <h1>Запишитесь на процедуру!</h1>
+          </div>
+          <form class="popup__form" method="get">
+            <input class="popup__form-text popup__form-input popup__form-fields" type="text" name="name" placeholder="Ваше имя" />
+            <input class="popup__form-tel popup__form-input popup__form-fields" type="tel" name="tel" placeholder="Номер телефона" />
+            <input class="popup__form-email popup__form-input popup__form-fields" type="email" name="email" placeholder="Электронная почта" />
+            <input class="popup__form-submit popup__form-input popup__form-button" type="submit" name="popup-submit" value="Отправить заявку" />
+          </form>
+        </div>
+      </div>
+    </div>
+    <script src="sidebar.js"></script>
+    <script src="jquery.min.js"></script>
+    <script src="form.js"></script>
+    <?php
+    if (isset($_GET['popup-submit'])) {
+      $nameform = $_GET['name'];
+      $telform = $_GET['tel'];
+      $emailform = $_GET['email'];
+      $mysqli = new mysqli("localhost", "root", "", "dudich");
+      if ($mysqli->connect__errno) {
+        echo "Извините, возникла проблема на сайте";
+        exit;
+      }
+      $name = '"' . $mysqli->real_escape_string($nameform) . '"';
+      $tel = '"' . $mysqli->real_escape_string($telform) . '"';
+      $email = '"' . $mysqli->real_escape_string($emailform) . '"';
+      $query = "INSERT INTO users (name, tel, email) VALUES ($name, $tel, $email)";
+      $result = $mysqli->query($query);
+      $mysqli->close();
+    }
+    ?>
+  </div>
+</body>
+
+</html>
